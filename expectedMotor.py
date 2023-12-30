8545ef0d3aae26c8a750857bc2577316495da28e
Aryan Karadia <105018373+aryan-karadia@users.noreply.github.com>
2023-12-29 18:55:04 -0700

    Update expectedMotor.py

--- expectedMotor.py
+++ expectedMotor.py
@@ -8,3 +8,14 @@ import math
 import ipywidgets as widgets
 import serial
 
+my_chain = ikpy.chain.Chain.from_urdf_file("simon-v2-0.urdf",active_links_mask=[False, False, True, True, True, True, True, True, True, True])
+
+def doIK():
+    global ik
+    old_position= ik.copy()
+    ik = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Z", initial_position=old_position)
+    
+def move(x,y,z):
+    global target_position
+    target_position = [x,y,z]
+    doIK()
