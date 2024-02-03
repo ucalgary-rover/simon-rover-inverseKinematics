858b3075065ba50ef774834650eb77612856ffc1
RoverSym <114888534+RoverSym@users.noreply.github.com>
2024-02-03 14:45:39 -0700

    Delete build directory

--- build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/magdy/Documents/MyGithub/simon-rover-inverseKinematics/build/turtlesim/turtlesim__py
-# Build directory: /home/magdy/Documents/MyGithub/simon-rover-inverseKinematics/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
